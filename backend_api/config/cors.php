b52340e05727244b2411dc4031086109bd4afa26
Minidu Oshan <126508461+MiniduOshan@users.noreply.github.com>
2026-06-25 19:51:40 +0530

    6th try for deploy backend

--- backend_api/config/cors.php
+++ backend_api/config/cors.php
@@ -2,11 +2,11 @@
 
 return [
 
-    'paths' => ['api/*', 'up'],
+    'paths' => ['*'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => array_filter(array_map('trim', explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:3000,http://localhost:5173,http://127.0.0.1:5173,http://localhost:50064')))),
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
