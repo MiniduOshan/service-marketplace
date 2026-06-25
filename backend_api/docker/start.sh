2b345fc62f0d49d82d2648e28bd7200c2091855b
Minidu Oshan <126508461+MiniduOshan@users.noreply.github.com>
2026-06-25 20:08:24 +0530

    8th try

--- backend_api/docker/start.sh
+++ backend_api/docker/start.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # Cache configuration
-php -d memory_limit=256M artisan config:cache
-php -d memory_limit=256M artisan route:cache
-php -d memory_limit=256M artisan view:cache
+php -d memory_limit=512M artisan config:cache
+php -d memory_limit=512M artisan route:cache
+php -d memory_limit=512M artisan view:cache
 
-# Run database migrations
-php -d memory_limit=256M artisan migrate --force
+# Run database migrations (don't crash if this fails)
+php -d memory_limit=512M artisan migrate --force || echo "WARNING: Migration failed, server will start anyway"
 
 # Start Supervisor (which starts Nginx and PHP-FPM)
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
