4671d6f14221f06a8480bc65bac1deaa83a337a0
Minidu Oshan <126508461+MiniduOshan@users.noreply.github.com>
2026-06-25 19:47:06 +0530

    5

--- backend_api/config/database.php
+++ backend_api/config/database.php
@@ -62,7 +62,7 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA', '/etc/ssl/certs/ca-certificates.crt'),
                 PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-            ]) : [],
+            ], function($val) { return $val !== null; }) : [],
         ],
 
         'mariadb' => [
